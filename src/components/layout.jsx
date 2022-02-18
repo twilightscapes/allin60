62a98cba779ae66591aa5dc2635bef62bc9b1081
twilightscapes <ihatespam@gmail.com>
2022-02-17 22:14:19 -0600

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -162,7 +162,7 @@ export function Layout({ children }) {
 
  <li className="carto" style={{border:'none', margin:'0 0',textAlign:'center'}}>
 <Link title="Return To Home" to="/" aria-label="Return To Home"> 
-<div className="TronType tronText" style={{fontSize:'150%', margin:'1rem 0 0 0'}}>TRON</div><div className="defont" style={{fontSize:'200%', fontWeight:'bold'}}>in 60</div><div className="TRON tronText" style={{fontSize:'100%', margin:'0 0 3rem 0'}}>Seconds</div>
+<div className="TronType tronText" style={{fontSize:'300%', margin:'1rem 0 0 0'}}>TRON</div><div className="defont" style={{fontSize:'200%', fontWeight:'bold'}}>in 60</div><div className="TRON tronText" style={{fontSize:'100%', margin:'0 0 3rem 0'}}>Seconds</div>
 {/* <div className="TRON tronlogo" style={{textAlign:'center', opacity:'1', textShadow:'2px 2px 10px 2px #000', maxWidth:'', color:'#fff', borderRadius:'0', fontSize:'clamp(4rem, 6vw, 5.8rem)', margin:'1rem 0 0 0'}}>[</div> */}
 {/* 
   <span>More Coming Soon</span> */}
