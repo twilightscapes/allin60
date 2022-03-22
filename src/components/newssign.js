8d796b56de29c42833761438d980d670bb49ac3a
twilightscapes <ihatespam@gmail.com>
2022-03-22 05:00:54 -0500

    LH2

--- src/components/newssign.js
+++ src/components/newssign.js
@@ -41,7 +41,7 @@ const NewsletterPage = () => (
 <CustomBox style={{}}> 
 
 
-<form  style={{width:'90%', margin:'0 auto', display:'flex', flexDirection:'column', justifyContent:'center', background: 'rgba(0,0,0,0.50)',
+<form  style={{width:'90%', margin:'0 auto 10vh auto', display:'flex', flexDirection:'column', justifyContent:'center', background: 'rgba(0,0,0,0.50)',
   backdropFilter: 'blur(4px)', border:'1px solid #000', borderRadius:'12px', textAlign:'center', alignSelf:'center', overflow:'hidden', color:'#fff'}}
           className="contact-form"
           action="/signedup/"
@@ -54,7 +54,7 @@ const NewsletterPage = () => (
 
 
             
-              <div className="txtshadow" style={{fontSize:'clamp(1rem, 1.5vw, 1.5rem)', textAlign:'center', marginTop:'10px'}}><strong>Get All The Latest Minutes</strong></div>
+              <div className="txtshadow" style={{fontSize:'clamp(1.4rem, 1.5vw, 1.5rem)', textAlign:'center', marginTop:'10px'}}><strong>Get All The Latest Minutes</strong></div>
               <br />
               <span className="txtshadow" style={{fontSize:'95%'}}>Get notified when NEW Minutes drop </span>
 
@@ -90,7 +90,7 @@ const NewsletterPage = () => (
             </div>
 
 
-            <div style={{fontSize: '', padding: '0px 3%', margin:'30px 0 10px 0', textAlign: 'center', color:'#ccc'}}>
+            <div style={{fontSize: '', padding: '0px 3%', margin:'30px 0 10px 0', textAlign: 'center', color:'#fff'}}>
             <Link to="/privacy/" className="" style={{textAlign: 'center', padding: '15px',  textDecoration: 'underline', border:'0px solid yellow'}}>privacy policy (NO SPAM!)</Link>
            
             </div>
